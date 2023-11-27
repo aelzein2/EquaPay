f2703bf5378f66878c8765bcc1327cc9a3b0b882
Ahmad El-Zein <aelzein2@uwo.ca>
2023-11-26 20:53:15 -0500

    Forgot password feature implemented
    Users can reset their password if forgotten. Only valid if user is already registered.

--- EquaPay/App.js
+++ EquaPay/App.js
@@ -9,11 +9,16 @@ import HomePage from './screens/HomePage';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
+  /* as of right now, you have to manually order the stack screens to test functionalities.
+    testing signup requires signup screen to be first. testing login/forgot password requires
+    login scren to be first. */
+    
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen options = {{headerShown : false}} name="Signup" component={Signup} />
       <Stack.Screen options = {{headerShown : false}} name="Login" component={Login} />
+      <Stack.Screen options = {{headerShown : false}} name="Signup" component={Signup} />
       <Stack.Screen name = "Homepage" component={HomePage} />
     </Stack.Navigator>
   </NavigationContainer>
