1f9188ffeeb68fafc1f2e3b154758a0d51e3ee4b
aalrawas <aalrawas@uwo.ca>
2023-11-29 22:33:51 -0500

    Helper functions, Server file, and logout function

--- EquaPay/screens/HomePage.js
+++ EquaPay/screens/HomePage.js
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Alert, Button } from "react-native";
+import { useState } from "react";
+import { useActionSheet } from "@expo/react-native-action-sheet";
+import { auth } from "../../firebase-config";
+import { signOut } from "firebase/auth";
 
 const HomePage = () => {
+  const bottomInset = useSafeAreaInsets().bottom;
+  const { showActionSheetWithOptions } = useActionSheet();
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+
+  const handleLogout = () => {
+    try {
+      showActionSheetWithOptions(
+        {
+          title: "Are you sure you want to logout?",
+          options: ["Logout", "Cancel"],
+          cancelButtonIndex: 1,
+          destructiveButtonIndex: 0,
+        },
+        async (buttonIndex) => {
+          if (buttonIndex === 0) {
+            setIsLoggingOut(true);
+            await signOut(auth);
+          }
+        }
+      );
+    } catch (err) {
+      Alert.alert(
+        "An error occurred while signing you out",
+        "Please try again later"
+      );
+    }
+  };
+
   return (
     <View>
       <Text>HomePage</Text>
+      {isLoggingOut ? (
+        <Text>Logging Out</Text>
+      ) : (
+        <Button 
+          text="Logout"
+          onPress={handleLogout}
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
---
+++ components/server.js
@@ -0,0 +1,5 @@
+let ip = ""; //for now, replace this with your ipv4 (use your own IP)
+
+const server = { url: "http://" + ip + ":8080" };
+
+export default server;
